e44e70bde213304ccc43154a9c0c9dd66d052fbc
Anh Chien Vu <vuanhchien003@gmail.com>
2024-09-27 23:45:17 -0400

    create POST request to post envelope and GET request to get all envelopes

--- index.js
+++ index.js
@@ -2,8 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Global variables to store envelopes and total budget
+let envelopes = [];
+let totalBudget = 0;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// POST /envelopes: create a new budget envelope
+app.post("/envelopes", (req, res) => {
+  const { name, budget } = req.body;
+
+  if (!name || !budget) {
+    return res.status(400).send("Name and budget are required");
+  }
+
+  const newEnvelope = { name, budget };
+  envelopes.push(newEnvelope);
+  totalBudget += budget;
+
+  res.status(201).send(newEnvelope);
+});
+
+// GET /envelopes: get all budget envelopes
+app.get("/envelopes", (req, res) => {
+  res.status(200).send(envelopes);
 });
 
 app.listen(port, () => {
